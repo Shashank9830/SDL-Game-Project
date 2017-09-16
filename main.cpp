0611a10ed18e0a93a6693bca8ab2eafe8e587944
Shashank Singh <shashank9830@users.noreply.github.com>
2017-09-16 10:59:31 +0530

    Completed main.cpp Build error is there

--- main.cpp
+++ main.cpp
@@ -38,4 +38,17 @@ int main(int argc, char* argv[])
 	//This function expects Red, Green, Blue and Alpha as color values
 	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
 
+	//clear the window to black
+	SDL_RenderClear(g_pRenderer);
+
+	//show the window
+	SDL_RenderPresent(g_pRenderer);
+
+	//set a delay before quitting
+	SDL_Delay(5000);
+
+	//clean up SDL
+	SDL_Quit();
+
+	return 0;
 }
