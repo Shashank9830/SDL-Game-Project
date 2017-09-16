2046873009a10e4031bc11c74c33b764afe63317
Shashank Singh <shashank9830@users.noreply.github.com>
2017-09-16 17:32:31 +0530

    Chapter 1 complete. update() function and Game class constructor & destructor has to be commented out for the code to work. freopen_s() is required and hence was removed from the code altogether.

---
+++ Game.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#ifndef __Game__
+#define __Game__
+
+#include "SDL.h"
+
+class Game
+{	
+	SDL_Window *m_pWindow;
+	SDL_Renderer *m_pRenderer;
+	bool m_bRunning;
+public:
+	/*Game();
+	~Game();*/
+
+	bool init(const char *title, int xpos, int ypos, int height, int width, bool fullscreen);
+
+	void render();
+	//void update();
+	void handleEvents();
+	void clean();
+
+	bool running()
+	{
+		return m_bRunning;
+	}
+};
+
+#endif /* defined(__Game__)*/
--- main.cpp
+++ main.cpp
@@ -1,54 +1,24 @@
-//Testing SDL Header files
+//Game is now divided into modules
 
-#include <iostream>
-#include <stdlib.h>
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_mixer.h>
-using namespace std;
+#include "Game.h"
+#include <Windows.h>
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
+//our Game Object
+Game *g_game = 0;
 
 int main(int argc, char* argv[])
-{
-	//initialize SDL
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+{	
+	g_game = new Game();
+
+	g_game->init("Chapter 1", 100, 100, 640, 580, false);
+
+	while (g_game->running())
 	{
-		//if succeeded create our window
-		g_pWindow = SDL_CreateWindow("Chapter 1 : Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
-
-		//if window creation succeeded create out renderer
-		if (g_pWindow != 0)
-		{
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
+		g_game->handleEvents();
+		//g_game->update();
+		g_game->render();
 	}
-	else
-	{
-		cout << "\n Could now initialize SDL, SDL Error : " << SDL_GetError << endl;
-		cout << "\n Exiting program...\n\n ";
-		system("pause");
-		exit(1);
-	}
-
-	//everything succeeded lets draw the window
-
-	//set to black
-	//This function expects Red, Green, Blue and Alpha as color values
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-
-	//clear the window to black
-	SDL_RenderClear(g_pRenderer);
-
-	//show the window
-	SDL_RenderPresent(g_pRenderer);
-
-	//set a delay before quitting
-	SDL_Delay(5000);
-
-	//clean up SDL
-	SDL_Quit();
+	g_game->clean();
 
 	return 0;
 }
