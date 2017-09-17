0bbd19b5fe72a124373ae7c3f11c0fb4b430dda4
Shashank Singh <shashank9830@users.noreply.github.com>
2017-09-17 13:47:00 +0530

    Created TextureManager.h header file

---
+++ TextureManager.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef __TextureManager__
+#define __TextureManager__
+
+#include "SDL.h"
+#include "SDL_image.h"
+#include <map>	//for std::map
+
+class Game
+{
+	std::map<std::string, SDL_Texture*> m_textureMap;
+
+public:
+
+	bool load(std::string filename, std::string id, SDL_Renderer *pRenderer);
+	
+	//draw
+	void draw(std::string id, int x, int y, int width, int height, SDL_Renderer *pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+	
+	//drawFrame
+	void drawFrame(std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+};
+
+#endif /* defined(__TextureManager__)*/
