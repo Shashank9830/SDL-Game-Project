b73beb9ef19fb86d9ca4efaabef8c7cb6096e771
Shashank Singh <shashank9830@users.noreply.github.com>
2017-09-29 20:01:03 +0530

    GameObjects is now an Abstract class

--- GameObject.h
+++ GameObject.h
@@ -3,27 +3,18 @@
 #ifndef __GameObject__
 #define __GameObject__
 #include <iostream>
-#include "SDL.h"
+#include "LoaderParams.h"
 
 class GameObject
 {
 protected:
-	std::string m_textureID;
-
-	int m_currentFrame;
-	int m_currentRow;
-
-	int m_x;
-	int m_y;
-
-	int m_width;
-	int m_height;
+	GameObject(const LoaderParams* pParams) {}
+	virtual ~GameObject(){}
 
 public:
-	virtual void load(int x, int y, int width, int height, std::string textureID);
-	virtual void draw(SDL_Renderer *pRenderer);
-	virtual void update();
-	virtual void clean();
+	virtual void draw() = 0;
+	virtual void update() = 0;
+	virtual void clean() = 0;
 };
 
 #endif //defined (__GameObject__)
