f2e90e2b7f52a77fc13a284135255e129a919a06
Shashank Singh <shashank9830@users.noreply.github.com>
2017-09-29 20:34:52 +0530

    SDLGameObject class is added for SDL specific code handling

---
+++ SDLGameObject.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#ifndef __SDLGameObject__
+#define __SDLGameObject__
+
+#include "GameObject.h"
+#include "LoaderParams.h"
+
+class SDLGameObject: public GameObject
+{
+protected:
+
+	int m_x;
+	int m_y;
+
+	int m_width;
+	int m_height;
+
+	int m_currentRow;
+	int m_currentFrame;
+
+	std::string m_textureID;
+
+public:
+
+	SDLGameObject(const LoaderParams* pParams);
+
+	virtual void draw();
+	virtual void update();
+	virtual void clean();
+};
+
+#endif //defined(__SDLGameObject__)
