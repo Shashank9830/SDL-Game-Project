7ad516ff48ae6a53871490cfface783e1afefd63
Shashank Singh <shashank9830@users.noreply.github.com>
2017-09-30 12:24:50 +0530

    SDLGameObject : m_position replaced m_x and m_y in constructor and draw()

--- SDLGameObject.cpp
+++ SDLGameObject.cpp
@@ -2,10 +2,8 @@
 #include "TextureManager.h"
 #include "Game.h"
 
-SDLGameObject::SDLGameObject(const LoaderParams* pParams): GameObject(pParams)
+SDLGameObject::SDLGameObject(const LoaderParams* pParams): GameObject(pParams), m_position(pParams->getX(), pParams->getY()))
 {
-	m_x = pParams->getX();
-	m_y = pParams->getY();
 	m_width = pParams->getWidth();
 	m_height = pParams->getHeight();
 
@@ -17,7 +15,7 @@ SDLGameObject::SDLGameObject(const LoaderParams* pParams): GameObject(pParams)
 
 void SDLGameObject::draw()
 {
-	TextureManager::Instance()->drawFrame(m_textureID, m_x, m_y, m_width, m_height, m_currentRow, m_currentFrame, TheGame::Instance()->getRenderer());
+	TextureManager::Instance()->drawFrame(m_textureID, (int)m_position.getX(), (int)m_position.getY(), m_width, m_height, m_currentRow, m_currentFrame, TheGame::Instance()->getRenderer());
 }
 
 void SDLGameObject::update()
