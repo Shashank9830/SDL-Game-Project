d25627045d4afd6fc887e2ea7604b7c28fbdc03d
Shashank Singh <shashank9830@users.noreply.github.com>
2017-09-30 12:34:13 +0530

    Modified the update() of Player and Enemy to animate the objects. Everything is working fine

--- Enemy.cpp
+++ Enemy.cpp
@@ -13,7 +13,7 @@ void Enemy::update()
 {
 	m_position.setX(m_position.getX() + 1);
 	m_position.setY(m_position.getY() + 1);
-	//m_currentFrame = int((SDL_GetTicks() / 100) % 6);
+	m_currentFrame = int((SDL_GetTicks() / 100) % 6);
 }
 
 void Enemy::clean()
--- Player.cpp
+++ Player.cpp
@@ -11,7 +11,7 @@ void Player::draw()
 void Player::update()
 {
 	m_position.setX(m_position.getX() - 1);
-	//m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
 }
 
 void Player::clean()
