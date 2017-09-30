960694643201b5fcd02fbb8fa8249f849d7bae46
Shashank Singh <shashank9830@users.noreply.github.com>
2017-09-30 12:54:16 +0530

    Velocity added to Player's update() function

--- Player.cpp
+++ Player.cpp
@@ -10,8 +10,11 @@ void Player::draw()
 
 void Player::update()
 {
-	m_position.setX(m_position.getX() - 1);
 	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
+
+	m_velocity.setX(1);
+
+	SDLGameObject::update();
 }
 
 void Player::clean()
