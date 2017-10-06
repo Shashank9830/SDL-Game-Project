b0cc7ffae014b372cdc343f180210697d723af9d
Shashank Singh <shashank9830@users.noreply.github.com>
2017-10-06 21:13:15 +0530

    GameState class is an Abstract Base class for all the Game states

---
+++ GameState.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef __GameState__
+#define __GameState__
+
+#include <string>
+
+class GameState
+{
+public:
+	virtual void update() = 0;
+	virtual void render() = 0;
+
+	virtual bool onEnter() = 0;
+	virtual bool onExit() = 0;
+
+	virtual std::string getStateID() const = 0;
+};
+
+#endif //defined(__GameState__)
