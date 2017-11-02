c0ed9807503c0a17032ac0151852e05d8d584f1f
Shashank Singh <shashank9830@users.noreply.github.com>
2017-11-02 21:54:21 +0530

    The image now flips based on velocity

--- SDLGameObject.cpp
+++ SDLGameObject.cpp
@@ -15,7 +15,15 @@ SDLGameObject::SDLGameObject(const LoaderParams* pParams): GameObject(pParams), 
 
 void SDLGameObject::draw()
 {
-	TextureManager::Instance()->drawFrame(m_textureID, (int)m_position.getX(), (int)m_position.getY(), m_width, m_height, m_currentRow, m_currentFrame, TheGame::Instance()->getRenderer());
+	//TextureManager::Instance()->drawFrame(m_textureID, (int)m_position.getX(), (int)m_position.getY(), m_width, m_height, m_currentRow, m_currentFrame, TheGame::Instance()->getRenderer());
+	if (m_velocity.getX() > 0)
+	{
+		TextureManager::Instance()->drawFrame(m_textureID, (Uint32)m_position.getX(), (Uint32)m_position.getY(), m_width, m_height, m_currentRow, m_currentFrame, TheGame::Instance()->getRenderer(), SDL_FLIP_HORIZONTAL);
+	}
+	else
+	{
+		TextureManager::Instance()->drawFrame(m_textureID, (Uint32)m_position.getX(), (Uint32)m_position.getY(), m_width, m_height, m_currentRow, m_currentFrame, TheGame::Instance()->getRenderer());
+	}
 }
 
 void SDLGameObject::update()
