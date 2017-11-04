b97684b89f3b86f0e201d4b685421c95945abfa5
Shashank Singh <shashank9830@users.noreply.github.com>
2017-11-04 14:02:49 +0530

    PlayState can now handle an enemy object (Helicopter 2)

--- PlayState.cpp
+++ PlayState.cpp
@@ -3,6 +3,7 @@
 #include "TextureManager.h"
 #include "Game.h"
 #include "Player.h"
+#include "Enemy.h"
 
 const std::string PlayState::s_playID = "PLAY";
 
@@ -29,9 +30,16 @@ bool PlayState::onEnter()
 		return false;
 	}
 
-	GameObject* player = new Player(new LoaderParams(100, 100, 128, 55, "helicopter"));
+	if (!TheTextureManager::Instance()->load("assets/helicopter2.png", "helicopter2", TheGame::Instance()->getRenderer()))
+	{
+		return false;
+	}
+
+	GameObject* player = new Player(new LoaderParams(500, 100, 128, 55, "helicopter"));
+	GameObject* enemy = new Enemy(new LoaderParams(100, 100, 128, 55, "helicopter2"));
 
 	m_gameObjects.push_back(player);
+	m_gameObjects.push_back(enemy);
 
 	std::cout << "entering PlayState\n";
 	return true;
