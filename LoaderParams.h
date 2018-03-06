8d9f487b42bef9d8ed4dc90db5013d7d4581bfe4
Shashank Singh <shashank9830@users.noreply.github.com>
2018-03-06 23:07:35 +0530

    Fixed m_numFrames in LoaderParameters.h

--- LoaderParams.h
+++ LoaderParams.h
@@ -12,17 +12,19 @@ class LoaderParams
 
 	int m_width;
 	int m_height;
+	int m_numFrames;
 
 	std::string m_textureID;
 
 public:
-	LoaderParams(int x, int y, int width, int height, std::string textureID) : m_x(x), m_y(y), m_width(width), m_height(height), m_textureID(textureID) { }
+	LoaderParams(int x, int y, int width, int height, int numFrames, std::string textureID) : m_x(x), m_y(y), m_width(width), m_height(height), m_numFrames(numFrames), m_textureID(textureID) { }
 
 	int getX() const { return m_x; }
 	int getY() const { return m_y; }
 	
 	int getWidth() const { return m_width; }
 	int getHeight() const { return m_height; }
+	int getnFrames() const { return m_numFrames; }
 	
 	std::string getTextureID() const { return m_textureID; }
 };
