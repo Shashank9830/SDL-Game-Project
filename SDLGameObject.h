1e6b0f696aca00ea66f2ced8faed35ced599b1a9
Shashank Singh <shashank9830@users.noreply.github.com>
2018-03-07 00:16:43 +0530

    Added minor functions in SDLGameObject

--- SDLGameObject.h
+++ SDLGameObject.h
@@ -30,6 +30,10 @@ public:
 	virtual void draw();
 	virtual void update();
 	virtual void clean();
+
+	Vector2D& getPosition() { return m_position; }
+	int getWidth() { return m_width; }
+	int getHeight() { return m_height; }
 };
 
 #endif //defined(__SDLGameObject__)
