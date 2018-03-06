3c137d209047c2246bcbaf6ca7685c65c6c817f5
Shashank Singh <shashank9830@users.noreply.github.com>
2018-03-06 23:52:51 +0530

    Added GameOverState.h

---
+++ GameOverState.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#ifndef __GameOverState__
+#define __GameOverState__
+
+#include "GameState.h"
+#include <string>
+#include <vector>
+
+class GameObject;
+
+class GameOverState : public GameState
+{
+	static void s_gameOverToMain();
+	static void s_restartPlay();
+
+	static const std::string s_gameOverID;
+
+	std::vector<GameObject*> m_gameObjects;
+public:
+
+	virtual void update();
+	virtual void render();
+
+	virtual bool onEnter();
+	virtual bool onExit();
+
+	virtual std::string getStateID() const { return s_gameOverID; }
+};
+
+#endif //defined!(__GameOverState__)
